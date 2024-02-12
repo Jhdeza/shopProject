891ed0cd430c78dd453a8c20574f58e1ca52cdd4
Jhdeza <javierhdeza1990@gmail.com>
2024-02-12 16:09:18 -0500

    contact 3

--- resources/views/information-crud/edit.blade.php
+++ resources/views/information-crud/edit.blade.php
@@ -13,11 +13,12 @@
                     <div class= "card-header">
                         <h3>@lang('main.contact_information')</h3>
                     </div>
-                    <form  enctype="multipart/form-data" class="form" method="post" action="
-                        @if($contacts_information->id!=null)
+                    <form enctype="multipart/form-data" class="form" method="post"
+                        action="
+                        @if ($contacts_information->id != null)
                        {{ route('information.update', $contacts_information->id) }}
-                       @else
-                       {{route('information.store')}}
+@else
+{{ route('information.store') }}
                        
                        ">
                         @csrf
@@ -29,11 +30,11 @@
 
                                 <input class="form-control " name="name_contact" type="text" placeholder="Name"
                                     value="{{ $contacts_information->name_contact }}">
-                                    @error('name_contact')
+                                @error('name_contact')
                                     <span style="display: block" class="error invalid-feedback ">
-                                        {{$message}}
+                                        {{ $message }}
                                     </span>
-                                    @enderror
+                                @enderror
 
                             </div>
 
@@ -43,26 +44,26 @@
 
                                 <input class="form-control" name="address_contacts" type="text"
                                     placeholder="Your Address" value="{{ $contacts_information->address_contacts }}">
-                                    @error('address_contacts')
+                                @error('address_contacts')
                                     <span style="display: block" class="error invalid-feedback ">
-                                        {{$message}}
+                                        {{ $message }}
                                     </span>
-                                    @enderror
+                                @enderror
 
                             </div>
-                           
+
 
                             <label class=" col-form-label">Email:</label>
                             <div class="form-group row">
 
 
                                 <input class="form-control" name="email" type="email" placeholder="Your Email"
-                                    value="{{ $contacts_information->email }}" >
-                                    @error('email')
+                                    value="{{ $contacts_information->email }}">
+                                @error('email')
                                     <span style="display: block" class="error invalid-feedback ">
-                                        {{$message}}
+                                        {{ $message }}
                                     </span>
-                                    @enderror
+                                @enderror
                             </div>
 
                             <label class="c col-form-label">Telefono:</label>
@@ -70,47 +71,47 @@
 
                                 <input class="form-control" name="phone_contacts" type="text" placeholder="Your Phone"
                                     value="{{ $contacts_information->phone_contacts }}">
-                                    @error('phone_contacts')
+                                @error('phone_contacts')
                                     <span style="display: block" class="error invalid-feedback ">
-                                        {{$message}}
+                                        {{ $message }}
                                     </span>
-                                    @enderror
+                                @enderror
                             </div>
                             <label class="c col-form-label">Redes Sociales:</label>
                             <div>
                                 <div class="form-group row">
-                                     <input class="form-control" name="social_facebook" type="text" placeholder="Facebook Link"
-                                    value="{{ $contacts_information->social_facebook }}">
-                               </div>
-                                <div class="form-group row">    
-                                    <input class="form-control" name="social_instagram" type="text" placeholder="Instagram Link"
-                                    value="{{ $contacts_information->social_instagram }}" >    
+                                    <input class="form-control" name="social_facebook" type="text"
+                                        placeholder="Facebook Link" value="{{ $contacts_information->social_facebook }}">
                                 </div>
-                                <div class="form-group row">                                    
-                                    <input class="form-control" name="social_twitter" type="text" placeholder="Twitter Link"
-                                    value="{{ $contacts_information->social_twitter }}">                                    
+                                <div class="form-group row">
+                                    <input class="form-control" name="social_instagram" type="text"
+                                        placeholder="Instagram Link" value="{{ $contacts_information->social_instagram }}">
+                                </div>
+                                <div class="form-group row">
+                                    <input class="form-control" name="social_twitter" type="text"
+                                        placeholder="Twitter Link" value="{{ $contacts_information->social_twitter }}">
                                 </div>
                             </div>
-                                
-                                <label class=" col-form-label">Descripción:</label>
+
+                            <label class=" col-form-label">Descripción:</label>
                             <div class="form-group message row">
 
                                 <textarea class="form-control" rows="5" name="description" placeholder="About Us">{{ $contacts_information->description }}</textarea>
                                 @error('description')
-                                <span style="display: block" class="error invalid-feedback ">
-                                    {{$message}}
-                                </span>
+                                    <span style="display: block" class="error invalid-feedback ">
+                                        {{ $message }}
+                                    </span>
                                 @enderror
                             </div>
                             <div class="form-group row">
                                 <div class="form-group">
                                     <x-image :params="[
-                                            'type' => 'simple',
-                                            'model' => $contacts_information,
-                                            'method' => 'imageUrl'
-                                        ]"/>
+                                        'type' => 'simple',
+                                        'model' => $contacts_information,
+                                        'method' => 'imageUrl',
+                                    ]" />
                                 </div>
-                                
+
                             </div>
 
 
@@ -126,15 +127,15 @@
             </div>
         </div>
     </div>
-    
+
 @endsection
 @push('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
     <script>
         $(document).on('click', 'input[type=button]', function(e) {
             e.preventDefault();
             let title = "Estas seguro que desea cambiar la informacion"
-             let form =$('form');
+            let form = $('form');
             swal({
                 title: title,
                 icon: "warning",
@@ -146,13 +147,11 @@
                 }
             });
         })
-        
-    </script>
-    <script>
 
-$(document).ready(function() {
-                
-    $('.file-upload').upload();
-            });
+
+        $(document).ready(function() {
+
+            $('.file-upload').upload();
+        });
     </script>
 @endpush
