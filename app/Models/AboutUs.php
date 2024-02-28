4ce43c9ad18a4bdb6452d158c1fa111016655654
Jhdeza <javierhdeza1990@gmail.com>
2024-02-28 12:52:01 -0500

    newfile

---
+++ app/Models/AboutUs.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class aboutUs extends Model
+{    use HasFactory;
+    
+    protected $guarded = [
+        "id"
+       
+        
+    ];
+    
+    public $timestamps = false;
+    const urlImageEmpty = 'path/to/empty/image.jpg';
+    protected $table = "about_us";
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function imageUrl(){
+        return $this->image_url;
+    }
+
+    public function getImageUrlAttribute(){
+        if($this->image)
+            return $this->image->url;
+        return self::urlImageEmpty;
+    }
+
+
+
+}
