d320b7813d0d4427d6b0a4d0dccc835b75e44472
Jhdeza <javierhdeza1990@gmail.com>
2024-02-27 22:35:35 -0500

    agregacion

--- resources/views/template/Pages/product-details.blade.php
+++ resources/views/template/Pages/product-details.blade.php
@@ -117,11 +117,11 @@
                                     <label @class(['title-label', 'd-none' => $product->quantity == 0]) id="title">@lang('main.stock1'):</label>
 
                                     <div class="form-control">
-                                           @if ($product->quantity > $product->quantity_alert)
+                                           @if ($product->quantity >= $product->quantity_alert)
                                          <span id="cantidad">@lang('main.stock')</span>
                                         @elseif($product->quantity === 0)
                                             <span id="cantidad">Agotado</span>
-                                        @elseif($product->quantity <= $product->quantity_alert)
+                                        @elseif($product->quantity < $product->quantity_alert)
                                         <span id="cantidad">{{$product->quantity}}</span>
                                        @endif
                                     </div>
