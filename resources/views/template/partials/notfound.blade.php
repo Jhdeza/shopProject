b83eefed71386aca3a4c2052db80e5f0e9a099a2
Jhdeza <javierhdeza1990@gmail.com>
2024-02-27 23:59:29 -0500

    arreglo nuevo

--- resources/views/template/partials/notfound.blade.php
+++ resources/views/template/partials/notfound.blade.php
@@ -1,11 +1,11 @@
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12 col-12 " style="min-height: 200px">
-            <div class="product-grids-head">
+<div class="container" style="min-height: 200px">
+    <div class="row align-items-center justify-content-center">
+        <div class="col-lg-12 col-12" >
+            <div class="product-grids-head text-center">
                 <h2>{{ __('main.no_products_available') }}</h2>
             </div>
             <br/>
-            <div class="product-grids-head">
+            <div class="product-grids-head text-center">
                 <h4>{{ __('main.no_products') }}</h4>
             </div>
         </div>
