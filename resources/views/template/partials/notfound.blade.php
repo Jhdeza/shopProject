13ed87acd3e260f8bd83da47dffb18d98f417b3c
Jhdeza <javierhdeza1990@gmail.com>
2024-02-27 22:30:05 -0500

    nuevo 1

--- resources/views/template/partials/notfound.blade.php
+++ resources/views/template/partials/notfound.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row">
-        <div class="col-lg-12 col-12">
+        <div class="col-lg-12 col-12 " style="min-height: 200px">
             <div class="product-grids-head">
                 <h2>{{ __('main.no_products_available') }}</h2>
             </div>
